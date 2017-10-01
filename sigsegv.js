c9f9caaa3dd97736c64235ec7bbc0490552c61f4
David Duong <david@duong.cz>
2017-10-01 11:02:02 +0200

    print logs to console

--- sigsegv.js
+++ sigsegv.js
@@ -7,7 +7,9 @@ if (!command) throw new Error('No command specified')
 const [name, ...args] = unquote(command).split(' ')
 const child = spawn(name, args)
 
+console.log('running', name, args)
 const handleOutput = (data) => {
+    console.log(data && data.toString && data.toString())
     if (data.toString().indexOf('SIGSEGV') > 0) {
         child.stdin.pause()
         child.kill('SIGINT')
