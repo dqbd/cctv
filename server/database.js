29b070d0379fc6cdf3f07d6f9f0850470cfd6efd
David Duong <david@duong.cz>
2017-12-24 12:05:14 +0100

    batch insert

--- server/database.js
+++ server/database.js
@@ -21,14 +21,21 @@ module.exports = class Cache {
         this.hasCreated[base] = true
     }
 
-    insertBulk(base, filenames) {
+    insertBulk(base, filenames, len = 3600) {
         this.createTable(base)
-        this.db.transaction(filenames.reduce((memo, filename) => {
-            const segment = createSegment(filename)
-            if (!segment) return memo
-            memo.push(`INSERT OR REPLACE INTO ${base} (filename, timestamp, duration, extinf) VALUES ('${segment.filename}', '${segment.timestamp}', '${segment.duration}', '${segment.extinf}')`)
-            return memo
-        }, [])).run()
+
+        let i = 0, n = filenames.length
+
+        while (i < n) {
+            console.log(`inserting batch ${(i+1) / len} of ${Math.ceil(n / len)}`)
+            this.db.transaction(filenames.slice(i, i += len).reduce((memo, filename) => {
+                const segment = createSegment(filename)
+                if (!segment) return memo
+                memo.push(`INSERT OR REPLACE INTO ${base} (filename, timestamp, duration, extinf) VALUES ('${segment.filename}', '${segment.timestamp}', '${segment.duration}', '${segment.extinf}')`)
+                return memo
+            }, [])).run()
+        }
+        
     }
 
     insert(base, filename) {
