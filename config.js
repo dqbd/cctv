8800ee12eb8a7d4e109480d663aee9642c7c8828
David Duong <david@duong.cz>
2018-03-04 18:29:10 +0100

    better config

--- config.js
+++ config.js
@@ -7,29 +7,29 @@ module.exports = {
     segmentSize: 3,
     port: 80,
     targets: {
-        VZADU_PROSTERADLA: {
-            name: "Vzadu prosteradla",
-            source: "rtsp://192.168.2.164:554/user=admin&password=&channel=1&stream=0.sdp?real_stream"
-        },
         VENKU: {
             name: "Venku",
             source: "rtsp://192.168.2.168:554/user=admin&password=&channel=1&stream=0.sdp?real_stream"
         },
-	STARY_VCHOD: {
-            name: "Stary vchod",
+        STARY_VCHOD: {
+            name: "Starý - vchod",
             source: "rtsp://192.168.2.172:554/user=admin&password=&channel=1&stream=0.sdp?real_stream"
-	},
-	STARY_SPODNI: {
-            name: "Stary spodni",
+        },
+        STARY_SPODNI: {
+            name: "Starý - spodní prádlo",
             source: "rtsp://192.168.2.176:554/user=admin&password=&channel=1&stream=0.sdp?real_stream"
-	},
-	VPREDU_VCHOD: {
-            name: "Vpredu vchod",
+        },
+        VPREDU_VCHOD: {
+            name: "Vpředu - vchod",
             source: "rtsp://192.168.2.180:554/user=admin&password=&channel=1&stream=0.sdp?real_stream"
-	},
-	VZADU_KOSILE: {
-	    name: "Vzadu kosile",
+        },
+        VZADU_KOSILE: {
+            name: "Vzadu - košile",
             source: "rtsp://192.168.2.184:554/user=admin&password=&channel=1&stream=0.sdp?real_stream"
-	}
+        },
+        VZADU_PROSTERADLA: {
+            name: "Vzadu - prostěradlo",
+            source: "rtsp://192.168.2.164:554/user=admin&password=&channel=1&stream=0.sdp?real_stream"
+        }
     }
 }
