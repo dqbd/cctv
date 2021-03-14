cbdd4915bfd7e82d897d064d152fdb26e9de32d8
Duong Tat Dat <david@duong.cz>
2021-03-14 02:01:47 +0100

    chore: add refresh image transition, hover, favicons

--- client/src/components/RefreshImg.tsx
+++ client/src/components/RefreshImg.tsx
@@ -9,6 +9,7 @@ export function RefreshImg({
   [rest: string]: any
 }) {
   const [session, setSession] = useState(Date.now())
+  const [visible, setVisible] = useState(false)
   useEffect(() => {
     const interval = window.setInterval(
       () => void setSession(Date.now()),
@@ -17,5 +18,14 @@ export function RefreshImg({
     return () => void window.clearInterval(interval)
   }, [])
 
-  return <img src={`${src}?q=${session}`} alt={alt} {...props} />
+  return (
+    <img
+      src={`${src}?q=${session}`}
+      alt={alt}
+      onLoad={() => setVisible(true)}
+      onError={() => setVisible(false)}
+      style={{ opacity: visible ? 1 : 0, transition: "all 0.3s" }}
+      {...props}
+    />
+  )
 }
